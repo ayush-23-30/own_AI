07ecc6c79e3efdde3f5a2a993e6de8ac3b21c9f5
Ayushreelax <146746299+ayush-23-30@users.noreply.github.com>
2025-01-24 20:47:06 +0530

    login/signUp page

---
+++ frontend/src/Screens/Register.jsx
@@ -0,0 +1,76 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import Button from '../custom/Button';
+import { IoEyeSharp } from 'react-icons/io5';
+import { FaEyeSlash } from 'react-icons/fa';
+
+function Register() {
+    const [eyes,setEyes] = useState(false); 
+  
+  
+  const toggle = ()=>{
+  setEyes(!eyes)
+  console.log(":", eyes)}
+
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gray-900 text-white">
+      <div className="w-full m-2 max-w-md p-8 space-y-6 bg-gray-800 rounded-lg shadow-lg">
+        <h2 className="text-3xl font-bold text-center">Sign Up</h2>
+       
+        <form className="space-y-4">
+          {/* Email Input */}
+          <div>
+            <label htmlFor="email" className="block mb-2 text-sm font-medium">
+              Email
+            </label>
+            <input
+              type="email"
+              id="email"
+              className="w-full px-4 py-2 text-gray-900 bg-gray-100 rounded-lg focus:ring-2 focus:ring-blue-500 focus:outline-none"
+              placeholder="Enter your email"
+              required
+            />
+          </div>
+          {/* Password Input */}
+          <div>
+            <label htmlFor="password" className="block mb-2 text-sm font-medium">
+              Password
+            </label>
+              <div className="relative w-full">
+                    <input
+                      type={eyes ? 'text' : 'password'}
+                          id="password"
+                          className="w-full px-4 py-2 text-gray-900 bg-gray-100 rounded-lg focus:ring-2 focus:ring-blue-500 focus:outline-none"
+                          placeholder="Enter your password"
+                          required
+                        />
+                      <div className='absolute bottom-3 right-3' onClick={toggle}>
+                      {eyes ? <IoEyeSharp color='black'  /> : <FaEyeSlash color='black'/>}
+            
+                      </div>
+            
+                      </div>
+          </div>
+          {/* Submit Button */}
+          <Button
+          type="submit"
+          // loading={loading}
+          className="w-full"
+        >
+       Sign Up
+        </Button>
+
+        </form>
+        {/* Navigation to Sign-Up */}
+        <p className="text-center text-sm">
+          Alreay have an account? 
+          <Link to="/login" className=" pl-1 text-blue-500 hover:underline">
+          login
+          </Link>
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default Register;
